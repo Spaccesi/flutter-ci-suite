b4d2122c052de94493e9632dabdf28455bc96c26
spaccesi <39572282+Spaccesi@users.noreply.github.com>
2026-03-25 14:40:15 +0100

    fix: update script paths for build_runner and gen-l10n actions

--- check/analyze/analyze.sh
+++ check/analyze/analyze.sh
@@ -1,6 +1,6 @@
 FLAGS=""
-[ "$ANALYZE_FAILS_IF_INFOS" == 'true' ] && FLAGS="$FLAGS --no-fatal-infos"
-[ "$ANALYZE_FAILS_IF_WARNINGS" == 'true' ] && FLAGS="$FLAGS --no-fatal-warnings"
+[ "$ANALYZE_FAILS_IF_INFOS" == 'false' ] && FLAGS="$FLAGS --no-fatal-infos"
+[ "$ANALYZE_FAILS_IF_WARNINGS" == 'false' ] && FLAGS="$FLAGS --no-fatal-warnings"
 [ "$NO_PUB" == 'true' ] && FLAGS="$FLAGS --no-pub"
 
 echo "▶️ Running flutter analyze with flags: $FLAGS"
